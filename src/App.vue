68b176a3f8e8e25206c0e5ebc573ad1f9b36350d
haqjahirul123 <haqjahirul1983@gmail.com>
2020-05-07 12:37:51 +0200

    jdhfjdhjh

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <add-blog></add-blog> 
+        <show-blog></show-blog>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Imports
+import addBlog from './components/addBlog.vue';
+import showBlog from './components/showBlog.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: {
+        'add-blog': addBlog,
+        'show-blog': showBlog
+    },
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
 }
 </style>
